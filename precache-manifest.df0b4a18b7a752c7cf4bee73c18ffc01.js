9974f71c8418fd5ca545424eda8e0d10e4149b1d
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2019-05-02 14:36:28 +0300

    try rebase branches

---
+++ precache-manifest.df0b4a18b7a752c7cf4bee73c18ffc01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc848c1056c3f9146e879963d4624d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02c58055fa2cdb1fb1e2",
+    "url": "/static/css/main.69a5e80c.chunk.css"
+  },
+  {
+    "revision": "0a2ae096b342c4024a89",
+    "url": "/static/js/2.b8c4cbcf.chunk.js"
+  },
+  {
+    "revision": "02c58055fa2cdb1fb1e2",
+    "url": "/static/js/main.21dbc05d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
